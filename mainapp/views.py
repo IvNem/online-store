801ac63ea61dc75e58ac4d69c3587c58ec66c258
IvNem <70778013+IvNem@users.noreply.github.com>
2021-01-07 23:20:36 +0300

    creating a template and order form

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,6 +5,9 @@ from django.http.response import HttpResponseRedirect
 from mainapp.models import Resistor, Transistor, Category, LatestProducts, Customer, Cart, CartProduct
 from .mixins import CategoryDetailMixin, CartMixin
 from django.contrib import messages
+from .forms import OrderForm
+from .utils import recount_cart
+from django.db import transaction
 
 
 class BaseView(CartMixin, View):
@@ -56,6 +59,7 @@ class CategoryDetailView(CartMixin, CategoryDetailMixin, DetailView):
         context['cart'] = self.cart
         return context
 
+
 class AddToCartView(CartMixin, View):
     def get(self, request, *args, **kwargs):
         # Берем нужные значения модели
@@ -71,7 +75,7 @@ class AddToCartView(CartMixin, View):
         # Проверка был ли создан продукт
         if created:
             self.cart.products.add(cart_product)
-        self.cart.save()
+        recount_cart(self.cart)
         messages.add_message(request, messages.INFO, 'Товар добавлен в корзину')
         return HttpResponseRedirect('/cart/')
 
@@ -90,7 +94,7 @@ class DeleteCartView(CartMixin, View):
         )
         self.cart.products.remove(cart_product)
         cart_product.delete()
-        self.cart.save()
+        recount_cart(self.cart)
         messages.add_message(request, messages.INFO, 'Товар удален из корзины')
         return HttpResponseRedirect('/cart/')
 
@@ -110,7 +114,7 @@ class ChangeQTYView(CartMixin, View):
         qty = int(request.POST.get('qty'))
         cart_product.qty = qty
         cart_product.save()
-        self.cart.save()
+        recount_cart(self.cart)
         messages.add_message(request, messages.INFO, 'Количество изменено')
         return HttpResponseRedirect('/cart/')
 
@@ -124,4 +128,45 @@ class CartView(CartMixin, View):
             'categories': categories
         }
         return render(request, 'cart.html', context)
+
+
+class CheckoutView(CartMixin, View):
+
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        form = OrderForm(request.POST or None)
+        context = {
+            'cart': self.cart,
+            'categories': categories,
+            'form': form
+        }
+        return render(request, 'checkout.html', context)
+
+
+class MakeOrderView(CartMixin, View):
+    @transaction.atomic
+    def post(self, request, *args, **kwargs):
+        form = OrderForm(request.POST or None)
+        customer = Customer.objects.get(user=request.user)
+        if form.is_valid():
+            # пример который нужно предзаполнить
+            new_order = form.save(commit=False)
+            new_order.customer = customer
+            new_order.first_name = form.cleaned_data['first_name']
+            new_order.last_name = form.cleaned_data['last_name']
+            new_order.phone = form.cleaned_data['phone']
+            new_order.address = form.cleaned_data['address']
+            new_order.buying_type = form.cleaned_data['buying_type']
+            new_order.order_date = form.cleaned_data['order_date']
+            new_order.comment = form.cleaned_data['comment']
+            # меняем статус корзины
+            self.cart.in_order = True
+            self.cart.save()
+            new_order.cart = self.cart
+            new_order.save()
+            # Список покупок
+            customer.orders.add(new_order)
+            messages.add_message(request, messages.INFO, 'Спасибо за заказ! Менеджер свяжется с Вами в ближайшее время')
+            return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/checkout/')
 
--- onlineStore/settings.py
+++ onlineStore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'mainapp',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static_dev'),
 )
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
